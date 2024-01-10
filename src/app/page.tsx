2a4952d864b8b19835cb345a7f10f1b0ab96a2d8
Thiago Dias <thiagodbrt@gmail.com>
2024-01-10 19:27:48 +0000

    Add base path to image source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { BASE_PATH } from "@/env";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -15,9 +16,9 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
-              src="/vercel.svg"
+              src={`${BASE_PATH}/vercel.svg`}
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +32,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={`${BASE_PATH}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
@@ -91,5 +92,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
